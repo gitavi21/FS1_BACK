5c93ad110f1d3ca4479c25fe5ce5d5e535755434
gitavi2108 <gitavi2108@gmail.com>
2024-06-19 00:17:28 +0530

    bcrypt fix

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const User = require('../models/user')
 
 const router = express.Router()
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const userControl = require('../controllers/user_control')
 router.post("/signin",userControl.createUser)
